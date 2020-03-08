7f71fd7975648d94be5ccc3574e8033fb18e34df
Pavel Tokarev <tokarevpavel1996@mail.ru>
2020-03-08 12:52:56 +0300

    Add function importXml() in 2.php Add DB 2.sql Add example.xml

--- 2.php
+++ 2.php
@@ -70,3 +70,117 @@ try
 	echo $e->getMessage();
 }
 ?>
+<br/>
+<?php
+$p = "example.xml";// Файл в той же папки где 2.php
+function importXml($a)
+{	
+	if (!file_exists($a))
+	{
+		echo " файл: $a не найден ";
+		return false;
+	}
+	$xml = simplexml_load_file($a); // Получаем объект XML			
+	$mysqli = new mysqli('127.0.0.1', 'root', '', 'test_samson');// Создаем объект MySQL
+	// Считываем данные из XML в массив $Продукт
+	$Продукт = [];
+	$i = 0;
+	foreach ($xml as $Товар)
+	{
+		$Продукт[$i]["Код"] = (int)$Товар->attributes()["Код"];
+		$Продукт[$i]["Название"] = (string)$Товар->attributes()["Название"];
+		foreach ($Товар->Цена as $Цена)
+		{			
+			$Продукт[$i]["Цена"][(string)$Цена->attributes()] = (int)$Цена;
+		}				
+		$Продукт[$i]["Свойства"] = (array)$Товар->Свойства;
+		$Продукт[$i]["Раздел"]=(array)$Товар->Разделы->Раздел;
+		$i++;
+	}
+	print_r ($Продукт);
+	//
+	// Таблица Категории
+	$Категории = [];
+	$j = 0;
+	foreach ($Продукт as $key => $attribut)
+	{	
+		for ($i=0; $i < count($attribut["Раздел"]); $i++)
+		{ 			
+			if (!in_array ($attribut["Раздел"][$i],$Категории))// Исключаем повторяющиеся категории
+			{				
+				$Категории[$j] = $attribut["Раздел"][$i];
+				$j++;
+			}
+		}		
+	}	
+	foreach ($Категории as $value) // Создаем sql запрос на добавлении Таблицы Категории
+	{		
+		$Название_кат ="'".$value."'";
+		$sql_proverka = "SELECT * FROM `a_category` WHERE `Название` = $Название_кат;";// SQL запрос на проверку наличия в БД
+		$sql_rez_prov = $mysqli->query($sql_proverka);		
+		if ($sql_rez_prov->num_rows==0)
+		{
+			$sql_cat .= "INSERT INTO `a_category` (`Название`,`id_parent`)
+			VALUES ($Название_кат,0);";
+			echo "Категория ".$value." успешно добавлена в БД \n";
+		} else
+		{
+			echo "Нельзя добавить в БД. Категория - ".$value." существует \n";
+		}		
+	}	
+	if(isset ($sql_cat))
+	{
+		$mysqli->multi_query($sql_cat);// Отправка sql запроса			
+	}	
+	$mysqli->close();
+	//
+	// Все остальные Таблицы
+    $mysqli = new mysqli('127.0.0.1', 'root', '', 'test_samson');	
+	foreach ($Продукт as $key => $attribut)
+	{
+		$Код = $attribut["Код"];		
+		$Название = "'".$attribut["Название"]."'";
+		$Раздел = "'".$attribut["Раздел"][0]."'";		
+		//Таблица Продукты
+		$sql_proverka = "SELECT * FROM `a_product` WHERE `Код` = $Код;";// SQL запрос на проверку наличия в БД
+		$sql_rez_prov = $mysqli->query($sql_proverka);	
+		if ($sql_rez_prov->num_rows==0)
+		{	
+			$sql_prod .= "INSERT INTO `a_product` (`Код`,`Название`) VALUES ($Код, $Название);";
+		}
+		else
+		{
+			echo "Товар с кодом = ".$Код." в БД существует \n";
+		}
+		//
+		//Таблица Свойства
+		$Значение_СВ = "";
+		foreach ($attribut["Свойства"] as $key_at=> $value)
+		{					
+			if (count($value)>1)
+			{
+				for ($i=0; $i < count($value); $i++)
+					{ 
+						$Значение_СВ .= $key_at." = ".$value[$i]." ";							
+					}
+			}else
+			{
+				$Значение_СВ .= $key_at." = ".$value." ";				
+			}			
+		}
+		$Значение_СВ = "'".$Значение_СВ."'";
+		$sql_id_cat = "SELECT `id` FROM `a_category` WHERE `Название` = $Раздел;";			
+		$sql_rez = $mysqli->query($sql_id_cat);		
+		$id_cat = $sql_rez->fetch_assoc()["id"];		
+		$sql_prop .= "INSERT INTO `a_property` (`Товар`,`Значение свойства`,`id_cat`) VALUES ($Название,$Значение_СВ,$id_cat);";
+		//Таблица Цена
+		foreach ($attribut["Цена"]  as $key_at => $Цена)
+		{
+			$Тип_цены ="'".$key_at."'";					
+			$sql_price .= "INSERT INTO `a_price` (`Связь товар`,`Тип цены`,`Цена`) VALUES ($Название,$Тип_цены,$Цена);";	
+		}					
+	}	
+	var_dump($mysqli->multi_query($sql_prod.$sql_prop.$sql_price));
+}
+importXml($p);
+?>
---
+++ 2.sql
@@ -0,0 +1,137 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1:3306
+-- Время создания: Мар 08 2020 г., 12:45
+-- Версия сервера: 5.7.25-log
+-- Версия PHP: 7.1.32
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `test_samson`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `a_category`
+--
+
+CREATE TABLE `a_category` (
+  `id` int(11) NOT NULL,
+  `Название` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_parent` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `a_price`
+--
+
+CREATE TABLE `a_price` (
+  `Связь товар` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Тип цены` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Цена` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `a_product`
+--
+
+CREATE TABLE `a_product` (
+  `id` int(11) NOT NULL,
+  `Код` int(11) NOT NULL,
+  `Название` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `a_property`
+--
+
+CREATE TABLE `a_property` (
+  `Товар` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `Значение свойства` text COLLATE utf8mb4_unicode_ci NOT NULL,
+  `id_cat` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `a_category`
+--
+ALTER TABLE `a_category`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `a_price`
+--
+ALTER TABLE `a_price`
+  ADD KEY `Связь товар` (`Связь товар`);
+
+--
+-- Индексы таблицы `a_product`
+--
+ALTER TABLE `a_product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `a_property`
+--
+ALTER TABLE `a_property`
+  ADD PRIMARY KEY (`Товар`),
+  ADD KEY `id_cat` (`id_cat`) USING BTREE;
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `a_category`
+--
+ALTER TABLE `a_category`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT для таблицы `a_product`
+--
+ALTER TABLE `a_product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `a_price`
+--
+ALTER TABLE `a_price`
+  ADD CONSTRAINT `a_price_ibfk_1` FOREIGN KEY (`Связь товар`) REFERENCES `a_property` (`Товар`);
+
+--
+-- Ограничения внешнего ключа таблицы `a_property`
+--
+ALTER TABLE `a_property`
+  ADD CONSTRAINT `a_property_ibfk_1` FOREIGN KEY (`id_cat`) REFERENCES `a_category` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
